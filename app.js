ab0a9ce3b65c77b9c0701248350b3e5c1bab4fdf
mehmet <tas349873@gmail.com>
2022-06-06 20:01:48 +0300

    js

---
+++ app.js
@@ -0,0 +1,39 @@
+let container = document.querySelector(".container");
+
+let result = document.querySelector(".result");
+
+let operation = document.getElementById("operation");
+
+let equal = document.querySelector(".equal");
+equal.addEventListener("click",() => {
+    myFunction();
+});
+
+container.addEventListener("click",(e) => {
+    if(e.target.classList.contains("AC")) {
+        result.innerHTML = "";
+        operation.innerHTML = "";
+    }
+
+
+
+if(!(e.target.innerText == "=" || e.target.innerText == "AC")) {
+    operation.textContent += e.target.textContent;
+}
+
+
+if(
+    e.target.innerHTML == "+" ||
+    e.target.innerHTML == "-" ||
+    e.target.innerHTML == "*" ||
+    e.target.innerHTML == "/" 
+) {
+    result.textContent += operation.textContent;
+    operation.textContent ="";
+}
+});
+
+function myFunction() {
+    result.textContent += operation.textContent;
+    operation.innerText = eval(result.innerText);
+}
